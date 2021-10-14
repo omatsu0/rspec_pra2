c10e08f4c85775d337aa7bd6e650e668c6feabf4
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-10-14 22:47:12 +0900

    usersにカラム追加して1個目テスト記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_141354) do
+ActiveRecord::Schema.define(version: 2021_10_14_134337) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
   end
 
 end
