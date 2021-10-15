ae505584896652a3752bd2efc2347ddee3606515
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-10-15 22:53:53 +0900

    devise追加とfirst_nameテスト追加

--- db/migrate/20211014134337_add_column_user.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :email, :string
-    add_column :users, :password, :string
-  end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # 姓、名、メール、パスワードがあれば有効な状態であること
-  it "is valid with a first name, last,name, email, and password" do
+  it "is valid with a first name, last name, email, and password" do
     user = User.new(
       first_name: "Aaron",
       last_name: "Sumner",
       email: "tester@example.com",
       password: "dottle-nouveau-pavilion-tights-furze",
-
     )
 
     expect(user).to be_valid
+
   end
 
   # 名がなければ無効な状態であること
-  it "is invalid without a first name"
+  it "is invalid without a first name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
 
   # 姓がなければ無効な状態であること
   it "is invalid without a last name"
