f5908e3755d67458a608f709e5a60c1d00cc819e
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-11-17 22:59:51 +0900

    P90

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 RSpec.describe ProjectsController, type: :controller do
 
   describe "#index" do
+    before do
+      @user = FactoryBot.create(:user)
+    end
     #正常にレスポンスを返すこと
     it "responds successfully" do
+      sign_in @user
       get :index
       expect(response).to be_success
     end
 
     it "returns a 200 response" do
+      sign_in @user
       get :index
       expect(response).to have_http_status "200"
     end
