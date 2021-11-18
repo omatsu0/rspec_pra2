bc913ef4243d50d3e1c9cbfb4a614baf6b1119a1
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-11-18 23:48:03 +0900

    P92

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 RSpec.describe ProjectsController, type: :controller do
 
   describe "#index" do
-    before do
-      @user = FactoryBot.create(:user)
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
     end
+
     #正常にレスポンスを返すこと
     it "responds successfully" do
       sign_in @user
@@ -19,6 +22,21 @@ RSpec.describe ProjectsController, type: :controller do
       expect(response).to have_http_status "200"
     end
 
+    # ゲストとして
+    context "as a guest" do
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        get :index
+        expect(response).to have_http_status "302"
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirects to the sign-in page" do
+        get :index
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+    
   end
 
 end
