85945ae5265a71d442242af940ec6927b2a062e8
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-11-19 22:22:49 +0900

    P94

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -36,7 +36,38 @@ RSpec.describe ProjectsController, type: :controller do
         expect(response).to redirect_to "/users/sign_in"
       end
     end
-    
   end
 
+  describe "#show" do
+    # 認可されたユーザーとして
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+    end
+
+    # 正常にレスポンスを返すこと
+    it "responds successfully" do
+      sign_in @user
+      get :show, params: { id: @project.id }
+      expect(response).to be_success
+    end
+  end
+
+  # 認可されていないユーザーとして
+  context "as an unauthorized user" do
+    before do
+      @user = FactoryBot.create(:user)
+      other_user = FactoryBot.create(:user)
+      @project = FactoryBot.create(:project, owner: other_user)
+    end
+
+    # ダッシュボードにリダイレクトすること
+    it "redirects to the dashboard" do
+      sign_in @user
+      get :show, params: { id: @project.id }
+      expect(response).to redirect_to root_path
+    end
+  end
 end
