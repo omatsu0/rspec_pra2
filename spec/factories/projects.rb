0ce95563862bd1a88ad279ea03c4a3aa861ed315
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-12-09 23:48:02 +0900

    p103途中

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -36,5 +36,10 @@ FactoryBot.define do
     # factory :project_due_tomorrow,class:Project do
     #   due_on {1.day.from_now}
     # end
+
+    # 無効になっている
+    trait :invalid do
+      name nil
+    end
   end
 end
