812f7c99e6a41d858e54d0fc89fe37f8b393eb4e
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-12-13 06:35:11 +0900

    104途中

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  before do
+    @user = FactoryBot.create(:user)
+    @project = FactoryBot.create(:project, owner: @user)
+    @task = @project.tasks.create!(name: "Test task")
+  end
 
 end
