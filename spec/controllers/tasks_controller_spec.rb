4fd3579c8060b9f3936bfade1d8172023bf38022
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-12-14 23:01:22 +0900

    104

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe TasksController, type: :controller do
     @task = @project.tasks.create!(name: "Test task")
   end
 
+  describe "#show" do
+    # JSON 形式でレスポンスを返すこと
+    it "responds with JSON formatted output" do
+      sign_in @user
+      get :show, format: :json,
+      params: { project_id: @project.id, id: @task.id }
+      expect(response.content_type).to eq "application/json"
+    end
+
 end
