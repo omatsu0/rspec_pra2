ed53251ac4fdae5e26cf27ea6db92965f208fa08
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2022-01-15 20:38:41 +0900

    143完

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -47,46 +47,17 @@ RSpec.describe Note, type: :model do
       )
     }
 
-  it "is valid with a user, project, and message" do
-    note = Note.new(
-      message: "This is a sample note.",
-      user: @user,
-      project: @project,
-    )
-
-    expect(note).to be_valid
-  end
-
-  it "is invalid without a message" do
-    note = Note.new(message: nil)
-    note.valid?
-    expect(note.errors[:message]).to include("can't be blank")
-    
-  end
-
-  describe "search message for a term" do
-    before do
-      @note1 = @project.notes.create(
-        message: "This is the first note.",
-        user: @user,
-      )
-      @note2 = @project.notes.create(
-        message: "This is the second note.",
-        user: @user,
-      )
-      @note3 = @project.notes.create(
-        message: "First, preheat the oven.",
-        user: @user,
-      )
-    end
-
+    # 一致するデータが見つかるとき
     context "when a match is found" do
+      # 検索文字列に一致するメモを返すこと
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1,@note3)
+        expect(Note.search("first")).to include(note1, note3)
       end
     end
 
+    # 一致するデータが1件も見つからないとき
     context "when no match is found" do
+      # 空のコレクションを返すこと
       it "returns an empty collection" do
         expect(Note.search("message")).to be_empty
       end
