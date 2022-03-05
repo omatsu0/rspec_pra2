c86df9b7ad11c310097150b048d61729c74373e5
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2022-03-05 23:08:44 +0900

    userのgeocodeを呼ぶテスト

--- spec/jobs/geocode_user_job_spec.rb
+++ spec/jobs/geocode_user_job_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe GeocodeUserJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # user の geocode を呼ぶこと
+  it "calls geocode on the user" do
+    user = instance_double("User")
+    expect(user).to receive(:geocode)
+    GeocodeUserJob.perform_now(user)
+  end
 end
